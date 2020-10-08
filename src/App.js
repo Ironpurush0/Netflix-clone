38fabd017125eb594d3fd21997fec00409b11dda
Ironpurush0 <ridamsaxena90.com>
2020-10-08 10:58:48 +0530

    first commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HeroComponent from './components/HeroComponent'
+import AboutComponent from './components/AboutComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <HeroComponent />
+    </div>
+  );
+}
+
+export default App;
