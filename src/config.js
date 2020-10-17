ab1dc0ff3a57d3eddef2487d346662f3b7b4d315
Ironpurush0 <ridamsaxena90.com>
2020-10-17 22:25:05 +0530

    .

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyCTVngfHh5bnT6k37LB3JXadVadcNpaZ2U",
+    authDomain: "netflix-clone-d4fd8.firebaseapp.com",
+    databaseURL: "https://netflix-clone-d4fd8.firebaseio.com",
+    projectId: "netflix-clone-d4fd8",
+    storageBucket: "netflix-clone-d4fd8.appspot.com",
+    messagingSenderId: "936527375738",
+    appId: "1:936527375738:web:c24eca661f225f32f33ac3",
+    measurementId: "G-7MNH3BWDTY"
+  }
--- src/firebaseConfig.prod.js
+++ src/firebaseConfig.prod.js
@@ -1,19 +1,11 @@
 import Firebase from 'firebase/app'
 import {seedDatabase} from './seed'
+import {config} from './config'
 
 import 'firebase/firebase-firestore'
 import 'firebase/firebase-auth'
 
-const config = {
-  apiKey: "AIzaSyCTVngfHh5bnT6k37LB3JXadVadcNpaZ2U",
-  authDomain: "netflix-clone-d4fd8.firebaseapp.com",
-  databaseURL: "https://netflix-clone-d4fd8.firebaseio.com",
-  projectId: "netflix-clone-d4fd8",
-  storageBucket: "netflix-clone-d4fd8.appspot.com",
-  messagingSenderId: "936527375738",
-  appId: "1:936527375738:web:c24eca661f225f32f33ac3",
-  measurementId: "G-7MNH3BWDTY"
-}
+
 
 const firebase = Firebase.initializeApp(config);
 
